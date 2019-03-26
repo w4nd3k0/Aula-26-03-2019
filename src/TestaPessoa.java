f9936e200d1a0ec5f5ebc79a6614033f185a9f71
ifnmg <ifnmg@192.168.163.19>
2019-03-26 20:39:49 -0300

    Aula 26-03

---
+++ src/TestaPessoa.java
@@ -0,0 +1,12 @@
+
+public class TestaPessoa {
+    
+    public static void main(String[] args) {
+        
+        Pessoa P1 = new Pessoa();
+        
+        P1.Nome = "Petrônio augusto";
+        P1.CPF = "123.456.789-00";
+        P1.Idade = 20;
+    }
+}
